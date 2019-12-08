8836b87de6bea10af05195149a883d417e4df7d4
Santiago Trini <santiagotrini@gmail.com>
2019-12-08 16:57:32 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express  = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/index');
+
+// config vars
+const db   = process.env.MONGODB_URI || 'mongodb://localhost/blog35';
+const port = process.env.PORT        || 3000;
+
+const app = express();
+
+// db connection
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`DB connected @ ${db}`);
+  })
+.catch(err => console.error(`Connection error ${err}`));
+
+// set views
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set routes
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
