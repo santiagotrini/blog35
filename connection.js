b49102a560d3347f59f36bbe7c85c68d9b2f0add
Santiago Trini <santiagotrini@gmail.com>
2019-12-09 11:13:26 -0300

    agrega MYSQL DBaaS

--- connection.js
+++ connection.js
@@ -1,8 +1,15 @@
 const mysql = require('mysql');
+let connection;
 
-module.exports = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'santiago',
-  password : '1234',
-  database : 'test'
-});
+if (process.env.MYSQL_URI) {
+  connection = mysql.createConnection(process.env.MYSQL_URI);
+} else {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'santiago',
+    password : '1234',
+    database : 'test'
+  });
+}
+
+module.exports = connection;
